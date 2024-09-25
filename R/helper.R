92a3feeba7cc0f2e5a94530cbc282a6fb94c5088
Tim Fullman <tfullman@tws.org>
2024-09-25 13:00:20 -0800

    add strtns() function

--- R/helper.R
+++ R/helper.R
@@ -27,6 +27,52 @@ calc_dist <- function(x.start, x.end, y.start, y.end){
   sqrt((x.end - x.start)^2 + (y.end - y.start)^2)
 }
 
+#' Calculate straightness of movement segment
+#'
+#' Helper function used by the \code{\link{BaBA_caribou}} function to calculate
+#' straightness of movement during an encounter as the ratio of Euclidean to
+#' path distance.
+#'
+#' @param mov_seg \code{\href{https://cran.r-project.org/package=sf}{sf}}
+#'   \code{POINT} object representing locations that are part of the encounter
+#'   being analyzed.
+#'
+#' @return Numeric value indicating the straightness of movement during the
+#'   encounter. Values range between 0-1, with values closer to 0 indicating
+#'   more sinuous movement.
+#' 
+strtns <- function(mov_seg) {
+  locs_tmp <- sf::st_coordinates(mov_seg)
+  if (sum(duplicated(mov_seg$date)) > 0 ) {
+    straightness <- NA
+  } else if(nrow(locs_tmp) == 1){
+    straightness <- NA
+  } else {
+    ## Calculate Euclidean distance between the first and last point
+    euc_dist <- 
+      as.numeric(
+        calc_dist(x.start = locs_tmp[1,1], x.end = locs_tmp[nrow(locs_tmp),1],
+                  y.start = locs_tmp[1,2], y.end = locs_tmp[nrow(locs_tmp),2]))
+    
+    ## Calculate path distance as the sum of all step lengths
+    mov_seg$dist <- NA
+    for(j in 2:nrow(mov_seg)){
+      mov_seg$dist[j] <- calc_dist(x.start = locs_tmp[j - 1, 1],
+                                   x.end = locs_tmp[j, 1],
+                                   y.start = locs_tmp[j - 1, 2],
+                                   y.end = locs_tmp[j, 2])
+    }
+    path_dist <- sum(mov_seg$dist, na.rm = TRUE)
+    
+    ## Calculate straightness as the ratio of Euclidean to path distance.
+    ## Straightness ranges from 0 to 1 with values closer to 0 being more
+    ## sinuous.
+    straightness <- euc_dist/path_dist
+  }
+  
+  return(straightness)
+}
+
 
 ## Helper function used by BaBA_default() to increase movement segment by one
 ## points before and one point after the focused encounter
